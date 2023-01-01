f5dc85864d88c79d9bcc88d83b952b73186da667
Eleanor Peng <eleanorpeng31@gmail.com>
2023-01-01 14:36:39 +0800

    Created API request

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -9,5 +9,62 @@ import Foundation
 
 /// Object that represents a single API call
 final class RMRequest {
+    /// API constants
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    /// Desired endpoint
+    private let endpoint: RMEndpoint
+    
+    /// Path components for API, if any
+    private let pathComponents: Set<String>
+    
+    /// Query components for API, if any
+    private let queryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api request in string format
+    private var urlString: String {
+        var base = Constants.baseUrl
+        base += "/"
+        base += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                base += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            base += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else {return nil}
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            base += argumentString
+        }
+        return base
+    }
+    /// Computed and constructed API url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    /// Desired HTTP method
+    public let httpMethod = "GET"
+    
+    /// Construct request
+    /// - Parameters:
+    ///   - endpoint: Target endpoint
+    ///   - pathComponents: Collection of Path components
+    ///   - queryParameters: Collection of query parameters
+    init(
+        endpoint: RMEndpoint,
+        pathComponents: Set<String> = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
